1a868c3949bdf8c09c91feb439a298e1b4edc160
tansi-jones <tansijones@outlook.com>
2022-06-17 22:55:57 +0300

    complete skeleton for widget

---
+++ components/Widgets.tsx
@@ -0,0 +1,36 @@
+import { SearchIcon } from "@heroicons/react/outline";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterMentionButton,
+  TwitterTweetEmbed,
+  TwitterMomentShare,
+  TwitterDMButton,
+  TwitterVideoEmbed,
+  TwitterOnAirButton,
+} from "react-twitter-embed";
+
+export const Widgets = () => {
+  return (
+    <div className="px-2 mt-2">
+      {/* Search box */}
+      <div className="flex items-center space-x-2 bg-gray-100 p-3 rounded-full mt-2">
+        <SearchIcon className="h-5 w-5 text-gray-400" />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Search Twitter"
+          className="bg-transparent outline-none flex-1"
+        />
+      </div>
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="Link_IO"
+        options={{ height: 1000 }}
+      />
+    </div>
+  );
+};
