dffae05d67c51290ac25d571a9dbc4e73955b1b2
tansi-jones <tansijones@outlook.com>
2022-06-17 23:09:45 +0300

    complete overall structure of page

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -2,9 +2,14 @@ import { RefreshIcon } from "@heroicons/react/outline";
 
 export const Feed = () => {
   return (
-    <div className="flex items-center justify-between">
-      <h1 className="p-5 text-xl font-bold pb-0">Home</h1>
-      <RefreshIcon className="h-8 w-8 cursor-pointer text-twitter mr-5 mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
+    <div className="col-span-7 lg:col-span-5 border-x">
+      <div className="flex items-center justify-between">
+        <h1 className="p-5 text-xl font-bold pb-0">Home</h1>
+        <RefreshIcon className="h-8 w-8 cursor-pointer text-twitter mr-5 mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
+      </div>
+
+      {/* Twitte box */}
+      <div className=""></div>
     </div>
   );
 };
--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -14,7 +14,7 @@ import {
 
 export const Widgets = () => {
   return (
-    <div className="px-2 mt-2">
+    <div className="hidden lg:inline px-2 mt-2 col-span-2">
       {/* Search box */}
       <div className="flex items-center space-x-2 bg-gray-100 p-3 rounded-full mt-2">
         <SearchIcon className="h-5 w-5 text-gray-400" />
