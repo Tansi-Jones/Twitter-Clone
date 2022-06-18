deeaeec13be14cb30c8342d39f6015a84a0f7c3b
tansi-jones <tansijones@outlook.com>
2022-06-18 13:46:04 +0300

    complete refreshing tweets

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,19 +1,35 @@
 import { RefreshIcon } from "@heroicons/react/outline";
 import { Tweet } from "../typings";
 import { TweetBox } from "./TweetBox";
-
+import toast from "react-hot-toast";
 import { Tweet as TweetComponent } from "../components/Tweet";
+import { fetchTweets } from "../utils/fetchTweets";
+import { useState } from "react";
 interface Props {
   tweets: Tweet[];
 }
 
-export const Feed = ({ tweets }: Props) => {
+export const Feed = ({ tweets: tweetsProp }: Props) => {
+  const [tweets, setTweets] = useState<Tweet[]>(tweetsProp);
   console.log(tweets);
+
+  const handleRefresh = async () => {
+    const refreshToast = toast.loading("Refreshing...");
+    const tweets = await fetchTweets();
+    setTweets(tweets);
+    toast.success("Feed Updated!", {
+      id: refreshToast,
+    });
+  };
+
   return (
     <div className="col-span-7 lg:col-span-5 border-x">
       <div className="flex items-center justify-between">
         <h1 className="p-5 text-xl font-bold pb-0">Home</h1>
-        <RefreshIcon className="h-8 w-8 cursor-pointer text-twitter mr-5 mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
+        <RefreshIcon
+          className="h-8 w-8 cursor-pointer text-twitter mr-5 mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125"
+          onClick={handleRefresh}
+        />
       </div>
 
       <div className="">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { Toaster } from "react-hot-toast";
 import { Feed } from "../components/Feed";
 import { Sidebar } from "../components/Sidebar";
 import { Widgets } from "../components/Widgets";
@@ -18,6 +19,7 @@ const Home: NextPage<Props> = ({ tweets }: Props) => {
         <title>Twitter Clone</title>
         <link rel="icon" href="https://links.papareact.com/drq" />
       </Head>
+      <Toaster />
 
       <main className="grid grid-cols-9">
         <Sidebar />
